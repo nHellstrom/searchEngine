4baa6404e81d94e60314658950363d5f27fc3b47
nHellstrom <nikhellstrom@hotmail.com>
2023-02-17 19:15:30 +0100

    Functional, not optimized

--- SearchEngine.Tests/UnitTest1.cs
+++ SearchEngine.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace SearchEngine.Tests;
 
 public class UnitTest1
 {
-    Searcher searcher = new();
+    Searcher searcher = new("doc1", "doc2", "doc3");
 
     [Fact]
     public void TokenizationShouldWork()
@@ -14,10 +14,39 @@ public class UnitTest1
         string testString = "General Kenobi. You are a bold one.";
 
         //Act
-        var results = searcher.Tokenizer(testString);
+        var results = searcher.Tokenize(testString);
 
         //Assert
-        string[] expected = new string[] {"General", "Kenobi", "You", "are", "a", "bold", "one"};
+        string[] expected = new string[] {"general", "kenobi", "you", "are", "a", "bold", "one"};
         Assert.Equal(expected, results);
+    }
+
+    [Fact]
+    public void IndexerShouldReturnDictioanry()
+    {
+        // Arrange
+        string doc1 = "Hello there";
+        string doc2 = "A Jedi uses the Force for knowledge and defense, never for attack";
+        string doc3 = "Are you threatening me, master Jedi?";
+        Dictionary<string, List<string>> testDictionary = new();
+
+        // Act
+        testDictionary = searcher.Index(doc1, testDictionary);
+        testDictionary = searcher.Index(doc2, testDictionary);
+        testDictionary = searcher.Index(doc3, testDictionary);
+
+        // Assert
+        List<string>? result = testDictionary.GetValueOrDefault("jedi");
+        List<string> expected = new List<string> {doc2, doc3};
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void SearchReturnsDocs()
+    {
+        string[] result = searcher.Search("fox");
+        string[] expected = {"doc1", "doc3"};
+        Assert.Equal(expected,result);
     }
 }
--- SearchEngine/Models/Models.cs
+++ SearchEngine/Models/Models.cs
@@ -1,11 +1,26 @@
-using System.Linq;
-
 namespace SearchEngine.Models;
 
 public class Searcher {
-    public string[] Tokenizer(string inputString) {
+    Dictionary<string, List<string>> searchIndex = new();
 
-        string[] trimmedStrangeChars = inputString.Select(x => char.IsLetter(x) ? x.ToString() : " ").ToArray();
+    public Searcher(params string[] documents) 
+    {
+        foreach (string document in documents) 
+        {
+            this.searchIndex = Index(document, searchIndex);
+        }
+    }
+
+    public string ReadDocument(string documentName)
+    {
+        string basePath = System.AppContext.BaseDirectory;
+        char separator = Path.DirectorySeparatorChar;
+        return System.IO.File.ReadAllText($"{basePath}{separator}Data{separator}{documentName}.txt");
+    }
+
+    public string[] Tokenize(string inputString) 
+    {
+        string[] trimmedStrangeChars = inputString.Select(x => char.IsLetter(x) ? x.ToString().ToLower() : " ").ToArray();
         string noStrangeChars = string.Join("", trimmedStrangeChars);
         string[] splitted = noStrangeChars.Split(" ");
 
@@ -18,7 +33,36 @@ public class Searcher {
                 holder.Add(x);
             } 
         }
-        
+
         return holder.ToArray();
     }
+
+    public Dictionary<string,List<string>> Index(string documentName, Dictionary<string,List<string>> index)
+    {
+        string documentText = ReadDocument(documentName);
+        string[] tokenizedDocument = Tokenize(documentText);
+
+        foreach (string word in tokenizedDocument) {
+            if (!index.ContainsKey(word))
+            {
+                index.Add(word, new List<string>{documentName});
+            }
+            else
+            {
+                index[word].Add(documentName);
+            }
+        }
+
+        return index;
+    }
+
+    public string[] Search(string searchTerm)
+    {
+        if (searchIndex.TryGetValue(searchTerm, out List<string>? result))
+        {
+            return result.ToArray();
+        }
+        return Array.Empty<string>();
+    }
+
 }
